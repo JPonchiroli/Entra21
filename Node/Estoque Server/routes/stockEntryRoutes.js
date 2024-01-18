1fd7e7a424390e007a9f9f29747b35cdf1811bf6
João PonchirolI <joaopedroponqueroli@gmail.com>
2024-01-18 16:42:21 -0300

    Adding SELECT ONE,  SELECT ALL Function and Postman Documentation

--- Node/Estoque Server/routes/stockEntryRoutes.js
+++ Node/Estoque Server/routes/stockEntryRoutes.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const router = express.Router()
 const stockEntry = require('../controllers/stockEntryController')
 
-router.get('/HelloStockEntry', stockEntry.HelloMessage) /*Debug Message*/ 
+router.get('/HelloStockEntry', stockEntry.HelloMessage) /*Debug Message */ 
 
 router.get('/StockEntry', stockEntry.ShowAllEntries)
 
