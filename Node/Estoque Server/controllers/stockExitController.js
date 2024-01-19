0b5d2724ec3f4b04412aad9881ce7a2b92fd185f
João PonchirolI <joaopedroponqueroli@gmail.com>
2024-01-19 14:56:29 -0300

    Creating the Stock Exit INSERT Function

--- Node/Estoque Server/controllers/stockExitController.js
+++ Node/Estoque Server/controllers/stockExitController.js
@@ -1,4 +1,6 @@
 const StockExit = require('../models/stockExit')
+const Product = require('../models/product')
+const StockEntry = require('../models/stockEntry')
 
 const stockExitControllers = {
     HelloMessage: async (req, res) => {
@@ -10,7 +12,22 @@ const stockExitControllers = {
     },
     CreateStockExit: async (req, res) => {
         try {
-            
+            const { id_product, quantity} = req.body
+            const product = await Product.findByPk(id_product)
+
+            if(!product) {
+                return res.status(404).send("Product Not Find")
+            }
+
+            const totalEntries = await StockEntry.sum('quantity', { where: { id_product } })
+            const totalExits = await StockExit.sum('quantity', { where: { id_product }})
+            const currentInventory = totalEntries - totalExits
+
+            if(quantity > currentInventory){
+                return res.status(400).send("The Stock is Insufficient")
+            }
+            await StockExit.create({ id_product, quantity, exit_date: new Date() })
+            res.send("The Stock Exit Registered Successfully")
         } catch (error) {
             res.status(500).send(error.message)
         }
